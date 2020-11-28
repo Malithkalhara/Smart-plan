7e0be91abed48c377900e1d40a12c2411e39ebdb
Malith Kalhara <malith.kalhara@randoli.ca>
2020-11-28 22:44:20 +0530

    Added report generate functionality

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,15 +8,33 @@ import Grid from '@material-ui/core/Grid';
 import FormGroup from '@material-ui/core/FormGroup';
 import Typography from '@material-ui/core/Typography';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 import Checkbox from '@material-ui/core/Checkbox';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { useForm } from 'react-hook-form';
 
 import Map from './map'
 
 const Home = () => {
 	const [latitude,setLatitude]=useState();
   const [longitude,setLongitude]=useState();
-  const [latt,setLat]=useState(7.488862);
-  const [lngg,setLng]=useState(80.353441);
+  const [latt,setLat]=useState(7.488862);//7.488862
+  const [lngg,setLng]=useState(80.353441);//80.353441
+  const [features, setFeatures] = useState({
+    school: [],
+    hospital: [],
+    bank: [],
+    police:[],
+    supermarket:[]
+  });
+  const [open, setOpen] = useState(false);
+  const { register } = useForm({
+    mode: 'onBlur',
+  });
   
   const center = {
     lat: parseFloat(latt),
@@ -37,13 +55,65 @@ const Home = () => {
   }
 
   const handleCheckbox = async (data,type) => {
-    console.log(data,type);
+
     if(data){
-      const result = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=7.488862,80.353441&radius=1500&type=${type}&key=AIzaSyCOEhXSw6tr5bfmCDTVGvOQaFfwkJ4XPWM`)
-      console.log(result.data.results);
+      if(data){
+        const result = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=${type}&key=AIzaSyCOEhXSw6tr5bfmCDTVGvOQaFfwkJ4XPWM`)
+        console.log(data,type);
+        console.log(result.data.results);
+
+        switch (type) {
+          case 'school':
+            setFeatures({...features,'school':result.data.results.slice(0,4)})
+            break;
+          case 'hospital':
+            setFeatures({...features,'hospital':result.data.results.slice(0,4)})
+            break;
+          case 'bank':
+            setFeatures({...features,'bank':result.data.results.slice(0,4)})
+            break;
+          case 'police':
+            setFeatures({...features,'police':result.data.results.slice(0,4)})
+            break;
+          case 'supermarket':
+            setFeatures({...features,'supermarket':result.data.results.slice(0,4)})
+            break;
+          default:
+            break;
+        }
+      }
+    }else{
+      switch (type) {
+        case 'school':
+          setFeatures({...features,'school':[]})
+          break;
+        case 'hospital':
+          setFeatures({...features,'hospital':[]})
+          break;
+        case 'bank':
+          setFeatures({...features,'bank':[]})
+          break;
+        case 'police':
+          setFeatures({...features,'police':[]})
+          break;
+        case 'supermarket':
+          setFeatures({...features,'supermarket':[]})
+          break;
+        default:
+          break;
+      }
     }
 
+
   }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
 
   return (
 		<Grid container>
@@ -54,18 +124,25 @@ const Home = () => {
       </Grid>
       <Grid item container style={{padding:"0px 100px 0px 340px"}} spacing={5}>
             <Grid item>
-              <TextField onChange={(e)=>{handleLatitude(e.target.value);}} label="Lantitude" variant="outlined" size="small" />
+              <TextField onChange={(e)=>{handleLatitude(e.target.value);}} label="Latitude" variant="outlined" size="small" />
             </Grid>
             <Grid item>
-              <TextField onChange={(e)=>{handleLongitude(e.target.value);}} label="Lantitude" variant="outlined" size="small" />
+              <TextField onChange={(e)=>{handleLongitude(e.target.value);}} label="Longitude" variant="outlined" size="small" />
             </Grid>
             <Grid item>
               <Button variant="contained" onClick={handleClick} color="primary">
                 Submit
               </Button>
             </Grid>
+            <Grid item style={{paddingLeft:'400px'}}>
+              <Button variant="contained" color="secondary" style={{height:'60px',}} onClick={handleClickOpen}>
+                Generate report
+              </Button>
+              
+            </Grid>
+            
       </Grid>
-      <Typography variant="subtitle1" gutterBottom style={{padding:"20px 100px 0px 340px"}}>
+      <Typography variant="subtitle1" gutterBottom style={{padding:"0px 100px 0px 340px"}}>
         Select Features :
       </Typography>
       <Grid item container style={{padding:"0px 100px 50px 340px"}} spacing={5}>
@@ -134,10 +211,247 @@ const Home = () => {
           label="Super markets"
           />
         </Grid>
-      </Grid>
+
+        </Grid>
       <div style={{padding:"0px 340px"}}>
-        <Map center={center}/>
+        <Map center={center} features={features}/>
       </div>
+      <Dialog
+        maxWidth='md'
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Basic Information</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText id="alert-dialog-description">
+           <Grid container >
+              <Grid item style={{flex:1}} container spacing={3}>
+                <Grid item  style={{flex:1}}>
+                  <p style={{color:'black'}}>Village</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="village"
+                      placeholder="Village"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>GND</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="gnd"
+                      placeholder="GND"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Grid item container spacing={3}>
+                <Grid item style={{flex:1}} >
+                <p style={{color:'black'}}>DSD</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="dsd"
+                      placeholder="DSD"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}> District</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="district"
+                      placeholder="District"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Grid item container spacing={3}>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Province</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="province"
+                      placeholder="Province"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Parcel ID</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="id"
+                      placeholder="Parcel ID"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Grid item container spacing={3}>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Lot No</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="lot"
+                      placeholder="Lot No"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Extent</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="extent"
+                      placeholder="Extent"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Grid item container spacing={3}>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Name of the land</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="name"
+                      placeholder="Name of the land"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Land use</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="land"
+                      placeholder="Land use"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Grid item container spacing={3}>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Claimant</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="claimant"
+                      placeholder="Claimant"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item style={{flex:1}}>
+                <p style={{color:'black'}}>Area (Acres/Roots/Perches)</p>
+                  <FormControl fullWidth>
+                    <TextField
+                      inputRef={register}
+                      name="area"
+                      placeholder="Area"
+                      variant="outlined"
+                      size='small'
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Grid item container>
+              <Grid item container>
+              <p style={{color:'black'}}>Facilities</p>
+              </Grid>
+                <Grid item style={{flex:1}}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        // checked={state.checkedB}
+                        //onChange={(e)=>{handleCheckbox(e.target.checked,'bank')}}
+                        name="checkedB"
+                        color="primary"
+                      />
+                    }
+                    label="Electricity"
+                  />
+                </Grid>
+                <Grid item style={{flex:1}}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        // checked={state.checkedB}
+                        //onChange={(e)=>{handleCheckbox(e.target.checked,'bank')}}
+                        name="checkedB"
+                        color="primary"
+                      />
+                    }
+                    label="Communication networks"
+                  />
+                </Grid>
+                <Grid item style={{flex:1}}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        // checked={state.checkedB}
+                        //onChange={(e)=>{handleCheckbox(e.target.checked,'bank')}}
+                        name="checkedB"
+                        color="primary"
+                      />
+                    }
+                    label="Trasportation facilities"
+                  />
+                </Grid>
+                <Grid item style={{flex:1}}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        // checked={state.checkedB}
+                        //onChange={(e)=>{handleCheckbox(e.target.checked,'bank')}}
+                        name="checkedB"
+                        color="primary"
+                      />
+                    }
+                    label="Sewage"
+                  />
+                </Grid>
+              </Grid>
+           </Grid>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            cancel
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Generate
+          </Button>
+        </DialogActions>
+      </Dialog>
         
 		</Grid>
   );
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -2,10 +2,14 @@ import React,{useState,useCallback} from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import { Marker } from '@react-google-maps/api';
 import { Circle } from '@react-google-maps/api';
+import { InfoWindow } from '@react-google-maps/api';
+import { InfoBox } from '@react-google-maps/api';
+import { DistanceMatrixService } from '@react-google-maps/api';
 
-const Map = ({center}) => {
+const Map = ({center,features}) => {
 
-  const [map, setMap] = useState(null)
+  const [distance, setDistance] = useState(null);
+  const [count, setCount] = useState('false')
 
   const containerStyle = {
     width: '1200px',
@@ -49,6 +53,72 @@ const Map = ({center}) => {
     radius: 2000,
     zIndex: 1
   }
+
+  console.log("Featuesssssss",features.school);
+
+  // const calculateDist=(locTo, locFrom)=> {
+  //   var lat2 = locTo.lat;
+  //   var lon2 = locTo.lng;
+  //   var lat1 = locFrom.lat;
+  //   var lon1 = locFrom.lng;
+  
+  //   var R = 6371; // km 
+  //   //has a problem with the .toRad() method below.
+  //   var x1 = lat2 - lat1;
+  //   var dLat = x1.toRad();
+  //   var x2 = lon2 - lon1;
+  //   var dLon = x2.toRad();
+  //   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+  //     Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+  //     Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  //   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  //   var d = R * c;
+  //   return d;
+  // }
+
+  // const getDistance = () =>{
+    
+  //   return(    
+  //     <DistanceMatrixService
+  //       options={{
+  //                 destinations: [feature.geometry.location],
+  //                 origins: [center],
+  //                 travelMode: "DRIVING",
+  //               }}
+  //       callback = {(response) => {
+  //         console.log('Distance',response)
+
+  //         setDistance(response.rows[0].elements[0].distance.text) 
+          
+  //       }}
+  //       />
+  //     )
+
+  // }
+
+  const testMarker=(feature,type)=>{
+    // const distance=calculateDist(feature.geometry.location,center)
+    //var distance='malith'
+      return (
+        <div>
+          <Marker position={feature.geometry.location}/>
+          <InfoWindow
+            //onLoad={onLoad}
+            position={feature.geometry.location}
+          >
+            <div >
+              <h3>{feature.name}</h3>
+              <h4></h4>
+            </div>
+          </InfoWindow>
+          
+        </div>
+        
+      )
+    
+
+  }
+  
  
   // const onLoad = useCallback(function callback(map) {
   //   const bounds = new window.google.maps.LatLngBounds();
@@ -68,10 +138,25 @@ const Map = ({center}) => {
         mapContainerStyle={containerStyle}
         center={center}
         zoom={14}
-        // onLoad={onLoad}
-        // onUnmount={onUnmount}
       >
         <Marker position={center}/>
+        
+        {features.school.map(school=>(
+          testMarker(school,'school')
+        ))}
+        {features.hospital.map(hospital=>(
+          testMarker(hospital,'hospital')
+          
+        ))}
+        {features.bank.map(bank=>(
+          testMarker(bank,'bank')
+        ))}
+        {features.police.map(police=>(
+          testMarker(police,'police')
+        ))}
+        {features.supermarket.map(supermarket=>(
+          testMarker(supermarket,'supermarket')
+        ))}
         <Circle
           options={options}
           center={center}
