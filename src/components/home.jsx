ca821bbfbf4e9cac838dbec86e3f387f8694330c
Malith Kalhara <malith.kalhara@randoli.ca>
2020-12-07 07:50:52 +0530

    updated report view

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -21,6 +21,8 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { useForm } from 'react-hook-form';
 import { Document, Page, Text, View, StyleSheet,PDFViewer } from '@react-pdf/renderer';
+import MaterialTable from 'material-table'
+import {Table,TableHeader,TableCell,TableBody,DataTableCell} from '@david.kucsai/react-pdf-table'
 
 import Map from './map'
 
@@ -42,6 +44,7 @@ const Home = () => {
   const { register ,getValues} = useForm({
     mode: 'onBlur',
   });
+  const [facilities, setFacilities] = useState([]);
   
   const center = {
     lat: parseFloat(latt),
@@ -114,6 +117,28 @@ const Home = () => {
 
   }
 
+  const handleFacilities = (type) =>{
+    switch (type) {
+      case 'school':
+        setFeatures({...features,'school':[]})
+        break;
+      case 'hospital':
+        setFeatures({...features,'hospital':[]})
+        break;
+      case 'bank':
+        setFeatures({...features,'bank':[]})
+        break;
+      case 'police':
+        setFeatures({...features,'police':[]})
+        break;
+      case 'supermarket':
+        setFeatures({...features,'supermarket':[]})
+        break;
+      default:
+        break;
+    }
+  }
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -137,6 +162,39 @@ const Home = () => {
     console.log(values);
     handleOpenPdf();
   };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
   const styles = StyleSheet.create({
     page: { width:'1000px',height:'1000px' },
@@ -485,6 +543,19 @@ const Home = () => {
                     label="Sewage"
                   />
                 </Grid>
+                <Grid item style={{flex:1}}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        // checked={state.checkedB}
+                        //onChange={(e)=>{handleCheckbox(e.target.checked,'bank')}}
+                        name="checkedB"
+                        color="primary"
+                      />
+                    }
+                    label="Water"
+                  />
+                </Grid>
               </Grid>
            </Grid>
           </DialogContentText>
@@ -517,20 +588,35 @@ const Home = () => {
                   <Text>Latitude : {latitude}</Text>
                   <Text>Longitude: {longitude}</Text>
               </View>
-              <View>
-                <Text>Village : {data.village}</Text>
-                <Text>GND : {data.gnd}</Text>
-                <Text>DSD : {data.dsd}</Text>
-                <Text>Dstrict : {data.district}</Text>
-                <Text>Province : {data.province}</Text>
-                <Text>Prcel ID : {data.id}</Text>
-                <Text>Lot No : {data.lot}</Text>
-                <Text>Extent : {data.extent}</Text>
-                <Text>Name of the land : {data.name}</Text>
-                <Text>Land use : {data.land}</Text>
-                <Text>Claimant : {data.claimant}</Text>
-                <Text>Area : {data.area}</Text>
-              </View>
+              <Table
+                    data={[
+                        {firstName: "Village", lastName: `${data.village}`},
+                        {firstName: "GND", lastName: `${data.gnd}`},
+                        {firstName: "DSD", lastName: `${data.dsd}`},
+                        {firstName: "District", lastName: `${data.district}`},
+                        {firstName: "Province", lastName: `${data.province}`},
+                        {firstName: "Parcel ID", lastName: `${data.id}`},
+                        {firstName: "Lot No", lastName: `${data.lot}`},
+                        {firstName: "Extent", lastName: `${data.extent}`},
+                        {firstName: "Name of the land", lastName: `${data.name}`},
+                        {firstName: "Land use", lastName: `${data.land}`},
+                        {firstName: "Claimant", lastName: `${data.claimant}`},
+                        {firstName: "Area", lastName: `${data.area}`},
+                    ]}
+                >
+                    <TableHeader>
+                        <TableCell>
+                            Name
+                        </TableCell>
+                        <TableCell>
+                            Value
+                        </TableCell>
+                    </TableHeader>
+                    <TableBody>
+                        <DataTableCell getContent={(r) => r.firstName}/>
+                        <DataTableCell getContent={(r) => r.lastName}/>
+                    </TableBody>
+                </Table>
             </Page>
           </Document>
         </PDFViewer>
